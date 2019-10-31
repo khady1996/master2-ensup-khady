e65e6f9bea74a144545583d8bba7258b7d5c162c
Khady NDIAYE <diijandiaye6@gmail.com>
2019-10-31 15:19:21 +0100

    multimoduling projet personnel

---
+++ src/main/java/eu/ensup/master2_ensup_khady/App.java
@@ -0,0 +1,13 @@
+package eu.ensup.master2_ensup_khady;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
